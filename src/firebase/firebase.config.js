ef9f67154fa1eddcc3ff33b2d91f4b34bed3c88b
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-05-01 21:15:47 +0600

    react router, firebase, tailwind install done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBqcjU_wDtpt_t-tOGUZfX-WEyldwVHh-U",
+  authDomain: "quince-2148f.firebaseapp.com",
+  projectId: "quince-2148f",
+  storageBucket: "quince-2148f.appspot.com",
+  messagingSenderId: "108734924495",
+  appId: "1:108734924495:web:de343887dd73aa2918d9ba"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
