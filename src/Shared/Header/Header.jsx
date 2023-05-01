040550604b889875642d00ce9eb164bf77ce2214
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-05-01 21:51:28 +0600

    navbar added

---
+++ src/Shared/Header/Header.jsx
@@ -0,0 +1,139 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div className='p-3'>
+            <nav className="relative px-4 py-4 flex justify-between items-center">
+                <Link className="text-3xl font-bold leading-none" to='/'>
+                    Quince
+                </Link>
+                <div className="lg:hidden">
+                    <button className="navbar-burger flex items-center text-gray-600 dark:text-gray-300 p-3">
+                        <svg
+                            className="block h-4 w-4 fill-current"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <title>Mobile menu</title>
+                            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+                        </svg>
+                    </button>
+                </div>
+                <ul className="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:items-center lg:w-auto lg:space-x-6">
+                    <li>
+                        <a
+                            className="text-sm text-gray-500 hover:text-gray-600 dark:text-gray-300"
+                            href="#"
+                        >
+                            Home
+                        </a>
+                    </li>
+                    <li className="text-gray-300">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            stroke="currentColor"
+                            className="w-4 h-4 current-fill"
+                            viewBox="0 0 24 24"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+                            />
+                        </svg>
+                    </li>
+                    <li>
+                        <a className="text-sm text-indigo-600 font-bold" href="#">
+                            About Us
+                        </a>
+                    </li>
+                    <li className="text-gray-300">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            stroke="currentColor"
+                            className="w-4 h-4 current-fill"
+                            viewBox="0 0 24 24"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+                            />
+                        </svg>
+                    </li>
+                    <li>
+                        <a
+                            className="text-sm text-gray-500 hover:text-gray-600 dark:text-gray-300"
+                            href="#"
+                        >
+                            Services
+                        </a>
+                    </li>
+                    <li className="text-gray-300">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            stroke="currentColor"
+                            className="w-4 h-4 current-fill"
+                            viewBox="0 0 24 24"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+                            />
+                        </svg>
+                    </li>
+                    <li>
+                        <a
+                            className="text-sm text-gray-500 hover:text-gray-600 dark:text-gray-300"
+                            href="#"
+                        >
+                            Pricing
+                        </a>
+                    </li>
+                    <li className="text-gray-300">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            stroke="currentColor"
+                            className="w-4 h-4 current-fill"
+                            viewBox="0 0 24 24"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+                            />
+                        </svg>
+                    </li>
+                    <li>
+                        <a
+                            className="text-sm text-gray-500 hover:text-gray-600 dark:text-gray-300"
+                            href="#"
+                        >
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+                <div className="space-x-2 hidden lg:block">
+                    <button className="rounded-md border border-indigo-600 px-3.5 py-1.5 text-base font-semibold leading-7 text-indigo-600 hover:bg-indigo-500 ">
+                        Login
+                    </button>
+                    <button className="rounded-md bg-indigo-600 px-3.5 py-1.5 text-base font-semibold leading-7 text-white hover:bg-indigo-500 ">
+                        SignUp
+                    </button>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
