dade33491d726a83c2c1c0a459502896633ea77d
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-05-02 07:54:08 +0600

    error page added

---
+++ src/Pages/Error/ErrorPage.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { FaArrowLeft } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+const ErrorPage = () => {
+    return (
+        <div className="py-10">
+            <div className="text-center">
+                <p className="text-base font-semibold text-orange-600">404</p>
+                <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-200 sm:text-5xl">
+                    Page not found
+                </h1>
+                <p className="mt-6 text-base leading-7 text-gray-600 dark:text-gray-400">
+                    Sorry, we couldn&apos:t find the page you&apos;re looking for.
+                </p>
+                <div className="flex items-center justify-center mt-6 gap-x-3">
+                    <Link to='/'>
+                        <button className="inline-flex items-center rounded-md border border-orange-600 px-3.5 py-1.5 text-base font-semibold leading-7 text-black hover:bg-orange-500 hover:text-white dark:text-white">
+                            <FaArrowLeft className="w-4 h-4 mr-2" />
+                            Take me Back
+                        </button>
+                    </Link>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
 import Blog from "../Pages/Blog/Blog";
+import ErrorPage from "../Pages/Error/ErrorPage";
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Main></Main>,
+        errorElement: <ErrorPage></ErrorPage>,
         children: [
             {
                 path: '/blog',
