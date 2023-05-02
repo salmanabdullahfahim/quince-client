6d5b9a00917fd8628aabeb2098196d74fa14f2c0
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-05-02 11:46:40 +0600

    chef recipie card added in the homepage

---
+++ src/Home/ChefCard/ChefCard.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const ChefCard = ({ chef }) => {
+    const { id, chefName, chefPicture, yearsOfExperience, likes, numberOfRecipes } = chef
+    return (
+            <div
+                className="rounded overflow-hidden shadow-lg dark:shadow-gray-800"
+            >
+                <img className="w-full" src={chefPicture} alt="Mountain" />
+                <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">{chefName}</div>
+                    <p className="text-gray-700 dark:text-gray-300 text-base">
+                        Expreience: {yearsOfExperience} years
+                    </p>
+                    <p className="text-gray-700 dark:text-gray-300 text-base">
+                        Number of Recipies: {numberOfRecipes}
+                    </p>
+
+                </div>
+                <div className="px-6 pt-2 pb-6 flex items-center justify-between">
+
+                    <button className="rounded-md bg-orange-500 px-3.5 py-1.5 text-base font-semibold leading-7 text-white hover:bg-orange-400 ">
+                        View Recipie
+                    </button>
+                    <button className='border border-black border-1 p-1 rounded-xl text-sm'>Likes {likes}</button>
+                </div>
+            </div>
+
+    );
+};
+
+export default ChefCard;
--- src/Home/Home/Home.jsx
+++ src/Home/Home/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 import { useSpring, animated } from 'react-spring';
+import ChefCard from '../ChefCard/ChefCard';
 
 const Home = () => {
 
@@ -9,41 +11,56 @@ const Home = () => {
         config: { duration: 1000 },
     });
 
+    const chefs = useLoaderData();
+
     return (
-        <div className="relative isolate pl-6 lg:pl-8 bg-white flex h-screen overflow-hidden mb-12 mt-1 p-4">
-            <div className="max-w-2xl py-32 sm:py-48 lg:py-32">
-                <div className="text-left">
-                    <animated.h1
-                        style={props}
-                        className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl"
-                    >
-                        Experience Culinary Excellence. Find Your Perfect Chef Today!
-                    </animated.h1>
-                    <p className="mt-6 text-lg leading-8 text-gray-600">
-                        Elevate Your Culinary Experience.Discover the Best Chefs. Find Your Perfect Chef Today.
-                    </p>
-                    <div className="mt-10 flex items-center gap-x-6">
-                        <a
-                            href="#"
-                            className="rounded-md bg-orange-500 px-3.5 py-2.5 text-sm font-semibold text-white"
+        <div>
+            <div className="relative isolate pl-6 lg:pl-8 bg-white flex h-screen overflow-hidden mb-12 mt-1 p-4">
+                <div className="max-w-2xl py-32 sm:py-48 lg:py-32">
+                    <div className="text-left">
+                        <animated.h1
+                            style={props}
+                            className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl"
                         >
-                            Hire a Chef Today
-                        </a>
-                        <a
-                            href="#"
-                            className="text-sm font-semibold leading-6 text-gray-900 hover:underline"
-                        >
-                            Get Started
-                        </a>
+                            Experience Culinary Excellence. Find Your Perfect Chef Today!
+                        </animated.h1>
+                        <p className="mt-6 text-lg leading-8 text-gray-600">
+                            Elevate Your Culinary Experience.Discover the Best Chefs. Find Your Perfect Chef Today.
+                        </p>
+                        <div className="mt-10 flex items-center gap-x-6">
+                            <a
+                                href="#"
+                                className="rounded-md bg-orange-500 px-3.5 py-2.5 text-sm font-semibold text-white"
+                            >
+                                Hire a Chef Today
+                            </a>
+                            <a
+                                href="#"
+                                className="text-sm font-semibold leading-6 text-gray-900 hover:underline"
+                            >
+                                Get Started
+                            </a>
+                        </div>
                     </div>
                 </div>
+                <div className="flex-1">
+                    <img
+                        className="max-h-full w-full object-cover rounded-lg"
+                        src="https://images.unsplash.com/photo-1577219491135-ce391730fb2c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2hlZnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
+                        alt="image"
+                    />
+                </div>
             </div>
-            <div className="flex-1">
-                <img
-                    className="max-h-full w-full object-cover rounded-lg"
-                    src="https://images.unsplash.com/photo-1577219491135-ce391730fb2c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2hlZnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
-                    alt="image"
-                />
+
+            <div>
+                <h2 className='text-center text-4xl font-bold'>Our Chefs</h2>
+            </div>
+
+            <div className="p-10 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-6">
+
+                {
+                    chefs.map(chef => <ChefCard key={chef.id} chef={chef}></ChefCard>)
+                }
             </div>
         </div>
     );
