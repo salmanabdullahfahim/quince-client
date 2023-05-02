869d97f9520ea76fe7136b856d7fedee67434cf6
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-05-02 15:55:57 +0600

    radial progress added in the loading state

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -2,11 +2,16 @@ import React, { useContext } from 'react';
 import { AuthContext } from '../Provider/AuthProvider';
 import { Navigate } from 'react-router-dom';
 
-const PrivateRoute = ({children}) => {
+const PrivateRoute = ({ children }) => {
 
-    const {user, loading} = useContext(AuthContext);
+    const { user, loading } = useContext(AuthContext);
 
-    if(user){
+    if (loading) {
+        return <div className='mx-auto text-center mt-12 mb-32'>
+            <div className="radial-progress" style={{ "--value": 90 }}>90%</div>
+        </div>
+    }
+    if (user) {
         return children;
     }
     return (<Navigate to='/login'></Navigate>);
